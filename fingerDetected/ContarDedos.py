5ba143e7d605a3fd9e739e12bb6072225d11dbe1
Kuro933 <federico.gonzalez@est.fi.uncoma.edu.ar>
2021-12-23 13:34:07 -0300

    minor fix: fix pos timer

--- fingerDetected/ContarDedos.py
+++ fingerDetected/ContarDedos.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import os
+import pyaudio
+import wave
 import ctypes
 import DetectarMano as htm
 import secuencia as s
@@ -8,30 +10,30 @@ import secuencia as s
 termino = True
 secuencia = s.secuencia()
 numero_intento = 0
+mano = "derecha"
 print(secuencia)
 cap = cv2.VideoCapture(1)
 wCam, hCam = 1366, 720
 cap.set(3, wCam)
 cap.set(4, hCam)
+tiempo_de_juego = 10
 k=0 
 reinicio = False
 derrota = True
 folderPath = "FingerImages"
 imagePath = r'C:\\Users\\gonzalezf\\Desktop\\git\\fiestaconfluencia\\fingerDetected\\image'
 myList = os.listdir(folderPath)
-# print(myList)
 overlayList = []
 for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
-    # print(f'{folderPath}/{imPath}')
     overlayList.append(image)
  
-# print(len(overlayList))
 pTime = 0
- 
+
+
 detector = htm.handDetector(detectionCon=0.90)
 start_time = time.time()
-tiempo_restante = 15
+tiempo_restante = tiempo_de_juego
 
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
@@ -58,31 +60,40 @@ while termino:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    # print(lmList)
     x_secuencia = 125
     y_secuencia = 40
-    if elapsed_time > 15 and derrota == True:
+    if elapsed_time > tiempo_de_juego and derrota == True:
         bad_img = cv2.imread(f'{imagePath}/derrota.jpg')
-        # cv2.imshow("Derrota", bad_img)
         reinicio = True
         derrota = False
-        tiempo_restante = 15
+        tiempo_restante = tiempo_de_juego
         numero_intento += 1
         Mbox('Derrota', f'Quedan {3 - numero_intento} intentos', 0)
         
 
 
-    
     if len(lmList) != 0:
         fingers = []
  
-        # Thumb
+        # pulgar derecho
         if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-            fingers.append(1)
+            mano = "derecha"
         else:
-            fingers.append(0)
+            mano = "izquierda"
  
-        # 4 Fingers
+        print("mano es ", mano)
+        if mano == 'derecha':
+            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        else:
+            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # 4 dedos
         for id in range(1, 5):
             if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
                 fingers.append(1)
@@ -99,18 +110,10 @@ while termino:
                 print("vamoo")
                 print("secuencia ", secuencia)
                 secuencia[k] = 'si'
-                #intentar poner un tilde
                 k += 1
         else:
             print("victoria")
-            # ret, frame = cap.read()
-            # hi,wi,_ = cv2.imread(f'{imagePath}/victoria.jpg').shape
-            # ok_img = cv2.imread(f'{imagePath}/victoria.jpg')
-            # frame[ 0:hi , 0:wi ] = ok_img
-            # cv2.imshow("Image", frame)
             ok_img = cv2.imread(f'{imagePath}/victoria.jpg')
-            # cv2.imshow("Victory", ok_img)
-            # time.sleep(2)
             reinicio = True
             numero_intento = 0
             Mbox('Victoria', 'Victoria!!', 0)
@@ -129,13 +132,15 @@ while termino:
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
+    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
     for num in secuencia:
         cv2.putText(img, f'{str(num)}', (x_secuencia,y_secuencia), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
         x_secuencia += 40
 
-    
-    cv2.putText(img, f'Tiempo restante: {int(tiempo_restante - elapsed_time)}', (20, 450), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
+
+    cv2.putText(img, f'Tiempo restante: {int(tiempo_restante - elapsed_time)}', (int(width*0.05), int(height*0.90)), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
  
     cv2.imshow("Image", img)
     cv2.waitKey(1)
