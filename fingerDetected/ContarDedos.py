701108e8cb3ce2e4b4ec055cda8e404e92a40175
Kuro933 <federico.gonzalez@est.fi.uncoma.edu.ar>
2022-01-20 10:56:01 -0300

    Minor fix: guardar foto del ganador

--- fingerDetected/ContarDedos.py
+++ fingerDetected/ContarDedos.py
@@ -6,13 +6,21 @@ import wave
 import ctypes
 import DetectarMano as htm
 import secuencia as s
- 
+from tkinter import *
+from tkinter import simpledialog
+
+ws = Tk()
+ws.title("ManoSecuencia")
+nombre = ""
+numero_telefono = ""
 termino = True
+inicio = True
+guardar = True
 secuencia = s.secuencia()
 numero_intento = 0
 mano = "derecha"
 print(secuencia)
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 wCam, hCam = 1366, 720
 cap.set(3, wCam)
 cap.set(4, hCam)
@@ -21,7 +29,9 @@ k=0
 reinicio = False
 derrota = True
 folderPath = "FingerImages"
-imagePath = r'C:\\Users\\gonzalezf\\Desktop\\git\\fiestaconfluencia\\fingerDetected\\image'
+sorteito = "sorteo"
+imagePath = r'C:\\Users\\gonzalezf\\Desktop\\git\\fiestaconfluencia\\SecuenciaDedos\\fingerDetected\\image'
+
 myList = os.listdir(folderPath)
 overlayList = []
 for imPath in myList:
@@ -42,6 +52,15 @@ tipIds = [4, 8, 12, 16, 20]
 e = 0
 while termino:
 
+    if inicio:
+        nombre = simpledialog.askstring("Input", "Nombre",parent=ws)
+        if nombre is not None:
+            numero_telefono = simpledialog.askstring("Input", "Número de telefono",parent=ws)
+            print("Tu nombre es: ", nombre)
+            print("Tu telefono es: ", numero_telefono)
+            inicio = False
+
+
     if numero_intento != 3:
         if reinicio:
             secuencia = s.secuencia()
@@ -52,8 +71,10 @@ while termino:
     else:
         Mbox("Derrota", "Lo siento se terminaron los intentos", 0)
         numero_intento=0
-        termino = False
-        cv2.destroyAllWindows()
+        inicio = True
+        guardar = True
+        # termino = False
+        # cv2.destroyAllWindows()
     
     
     elapsed_time = time.time() - start_time
@@ -70,29 +91,14 @@ while termino:
         numero_intento += 1
         Mbox('Derrota', f'Quedan {3 - numero_intento} intentos', 0)
         
-
-
     if len(lmList) != 0:
         fingers = []
  
-        # pulgar derecho
         if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-            mano = "derecha"
+            fingers.append(1)
         else:
-            mano = "izquierda"
- 
-        print("mano es ", mano)
-        if mano == 'derecha':
-            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-        else:
-            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
+            fingers.append(0)
+     
         # 4 dedos
         for id in range(1, 5):
             if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
@@ -115,6 +121,8 @@ while termino:
             print("victoria")
             ok_img = cv2.imread(f'{imagePath}/victoria.jpg')
             reinicio = True
+            inicio = True
+            guardar = True
             numero_intento = 0
             Mbox('Victoria', 'Victoria!!', 0)
             
@@ -135,12 +143,21 @@ while termino:
     width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
+    if guardar:
+        if ((int)(tiempo_restante - elapsed_time)) == 5:
+            cv2.putText(img, f'Nombre: {str(nombre)}', (int(width*0.5), int(height*0.80)), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
+            cv2.putText(img, f'Telefono: {str(numero_telefono)}', (int(width*0.5), int(height*0.90)), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
+            cv2.imwrite(f'{sorteito}/{nombre}.jpg',img)
+            guardar = False
+
+
     for num in secuencia:
         cv2.putText(img, f'{str(num)}', (x_secuencia,y_secuencia), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
         x_secuencia += 40
 
 
     cv2.putText(img, f'Tiempo restante: {int(tiempo_restante - elapsed_time)}', (int(width*0.05), int(height*0.90)), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
- 
+    
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
